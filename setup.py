fd2b16a4736c66920fbd58fd7ea0f9fc4bb4eec3
vchhajed <vchhajed@buffalo.edu>
2024-03-18 18:15:12 -0700

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup, find_packages
 
+# Read the content of your README file
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='graphlit_client',
-    version='0.1',
+    version='0.1.2',
     packages=find_packages(),
     install_requires=[
         'PyJWT',
@@ -11,4 +15,6 @@ setup(
     author_email='questions@graphlit.com',
     description='A package for creating tokens for Graphlit services',
     url='https://github.com/graphlit/graphlit-client-python',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
