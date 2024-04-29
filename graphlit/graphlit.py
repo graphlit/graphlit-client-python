06b9465827e9451085bb1c7d511dade20ba57f18
Kirk Marple <kirk@graphlit.com>
2024-04-29 10:16:57 -0700

    .

--- graphlit/graphlit.py
+++ graphlit/graphlit.py
@@ -2,6 +2,8 @@ import os
 import jwt
 import datetime
 import httpx
+import asyncio
+import random
 from graphlit_api.client import Client
 
 class Graphlit:
@@ -10,13 +12,25 @@ class Graphlit:
         self.environment_id = environment_id if environment_id is not None else os.getenv("GRAPHLIT_ENVIRONMENT_ID")
         self.owner_id = owner_id if owner_id is not None else os.getenv("GRAPHLIT_OWNER_ID")
         self.secret_key = jwt_secret if jwt_secret is not None else os.getenv("GRAPHLIT_JWT_SECRET")
-
         self.api_uri = api_uri if api_uri is not None else "https://data-scus.graphlit.io/api/v1/graphql/"
+        
+        self.token = self.generate_jwt()
 
-        # Specify the expiration (one hour from now in UTC)
+        headers = {"Authorization": f"Bearer {self.token}"}
+        
+        self.client = Client(url=self.api_uri, headers=headers)
+        self.client.http_client.timeout = httpx.Timeout(timeout=600.0)
+        self.client.event_hooks = {'response': [self.exponential_backoff]}
+        
+        self.retry_state = {
+            "attempts": 0,
+            "max_attempts": 7,
+            "base_delay": 1  # Base delay in seconds
+        }
+
+    def generate_jwt(self):
         expiration = datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(hours=1)
-
-        # Define the payload
+        
         payload = {
             "https://graphlit.io/jwt/claims": {
                 "x-graphlit-organization-id": self.organization_id,
@@ -30,11 +44,16 @@ class Graphlit:
 
         if self.owner_id is not None:
             payload["https://graphlit.io/jwt/claims"]["x-graphlit-owner-id"] = self.owner_id
+        
+        return jwt.encode(payload, self.secret_key, algorithm="HS256")
 
-        # Sign the JWT
-        self.token = jwt.encode(payload, self.secret_key, algorithm="HS256")
+    async def exponential_backoff(self, request: httpx.Request, response: httpx.Response):
+        if response.status_code >= 500 and self.retry_state["attempts"] < self.retry_state["max_attempts"]:
+            self.retry_state["attempts"] += 1
+            
+            delay = self.retry_state["base_delay"] * (2 ** (self.retry_state["attempts"] - 1))
+            delay += random.uniform(0, self.retry_state["base_delay"])
+            
+            await asyncio.sleep(delay)
 
-        headers = {"Authorization": f"Bearer {self.token}"}
-
-        self.client = Client(url=self.api_uri, headers=headers)
-        self.client.http_client.timeout = httpx.Timeout(timeout=600.0)
+            return await response.anext()
