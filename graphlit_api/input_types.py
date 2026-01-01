c875d790eebdb2312134eb4d8609c19de3c4c492
Kirk Marple <kirk@graphlit.com>
2026-01-01 13:49:15 -0800

    Updated to latest API release.

--- graphlit_api/input_types.py
+++ graphlit_api/input_types.py
@@ -365,6 +365,7 @@ class OAuthAuthenticationPropertiesInput(BaseModel):
     client_id: str = Field(alias="clientId")
     client_secret: str = Field(alias="clientSecret")
     refresh_token: str = Field(alias="refreshToken")
+    redirect_uri: Optional[str] = Field(alias="redirectUri", default=None)
     metadata: Optional[str] = None
 
 
